10d3cad522e000c3f0205344b04546e47f1f644b
Jim Lloyd <jim.lloyd@etagen.com>
2023-09-21 05:16:17 +0000

    renderYaml()

--- lib/json.ts
+++ lib/json.ts
@@ -1,6 +1,13 @@
+import { stringify } from 'yaml'
+
 export type JSONValue =
     | string
     | number
     | boolean
     | { [x: string]: JSONValue }
     | Array<JSONValue>
+
+export function renderYaml(val: JSONValue): void
+{
+    console.log(stringify(val))
+}
