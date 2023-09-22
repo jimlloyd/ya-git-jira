e8dbf957c47f0fdbc6f7853f71b293b3ca8a7ab5
Jim Lloyd <jim.lloyd@etagen.com>
2023-09-22 02:55:19 +0000

    git: MergeTrain types and transformations

--- lib/gitlab/merge-trains.ts
+++ lib/gitlab/merge-trains.ts
@@ -1,7 +1,7 @@
 import { projectScopedGet } from './project'
 import type { JSONValue } from '../json'
 
-export type MergeTrain = JSONValue & {
+export type MergeTrainBrief = {
     id: number
     merge_request: {
         title: string
@@ -14,6 +14,24 @@ export type MergeTrain = JSONValue & {
     status: string
 }
 
+export type MergeTrainSimple = {
+    title: string
+    target_branch: string
+    username: string
+}
+
+export type MergeTrain = JSONValue & MergeTrainBrief
+
 export async function getMergeTrains(): Promise<MergeTrain[]> {
     return await projectScopedGet(`merge_trains`) as MergeTrain[]
+}
+
+export function briefly_one(train: MergeTrain) : MergeTrainSimple {
+    const { target_branch, merge_request: { title }, user: { username} } = train
+    return { title, target_branch, username }
+}
+
+export function briefly_list(trains: MergeTrain[]) : MergeTrainSimple[]
+{
+    return trains.map(briefly_one)
 }
